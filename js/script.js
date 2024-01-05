9f28ee8675d7056a9eb75147404e9a2a97fb0267
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2024-01-05 18:12:06 +0530

    operators

---
+++ js/script.js
@@ -0,0 +1,54 @@
+//to use console check the below line
+console.log("welcome to js");
+console.log(20);
+console.log(35.3);
+// to print objects
+console.log({fname:"kiruthiga", gender:"female",age:26});
+//to print array
+console.log([10,12,13,14,14]);
+//to print array in table format
+console.table([10,12,13,14,15]);
+//to print object in table format
+console.table({fname:"Anu",gender:"female",age:28});
+
+//to print the values in different variable
+
+var a=5;
+let b=10;
+const c=15;
+console.log(5);
+console.log(a);
+console.log(b);
+console.log(c);
+
+a=6;
+console.log(a);
+b=7;
+console.log(b);
+
+a=8.5;
+console.log(a);
+console.log(a,typeof a);
+
+a="8.5";
+console.log(a);
+console.log(a,typeof a);
+
+
+
+
+// Arithmetic operators
+console.log("print the value of a=",+a);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//relational operators
+console.log(a<b);
+console.log(a>c);
+console.log(a==c);
+console.log(a>=c);
+console.log(a<=b);
+
